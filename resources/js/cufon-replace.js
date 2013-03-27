8233262abb58d209d5f7b825df6ca1f98bd44a4a
JGtm <g.sit@hotmail.fr>
2013-03-27 13:59:29 +0100

    Interface Karma

---
+++ resources/js/cufon-replace.js
@@ -0,0 +1,5 @@
+Cufon.replace('.logo', { fontFamily: 'James Fajardo' });
+Cufon.replace('h3', { fontFamily: 'Caviar Dreams' });
+Cufon.replace('h2', { fontFamily: 'Caviar Dreams' });
+Cufon.replace('h4', { fontFamily: 'Caviar Dreams', textShadow:'0 0 #4a4a4a' });
+Cufon.replace('.block1 h3', { fontFamily: 'Caviar Dreams', textShadow:'0 0 #0df7ff, 0 0 #0df7ff, 0 0 #0df7ff, 0 0 #0df7ff, 0 0 #0df7ff'});
