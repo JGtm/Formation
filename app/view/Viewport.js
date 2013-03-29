1e220a7bc5cf6ccbf475d8a008c75f5a1c8666b4
JGtm <g.sit@hotmail.fr>
2013-03-29 11:46:12 +0100

    Rien

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -1,22 +1,22 @@
-//Ext.define('Formation.view.Viewport', {
-//    renderTo: Ext.getBody(),
-//    extend: 'Ext.container.Viewport',
-//    requires:[
-//    'Ext.tab.Panel',
-//    'Ext.layout.container.Border'
-//    ],
-//
-//    layout: {
-//	type: 'ux.center'
-//    },
-//
-//    items: [{
-//	xtype: 'panel',
-//	title: 'BIENVENUE PAR ICI COPAIN',
-//	width: 800,
-//	items:[{
-//	    xtype: 'tabpanel',
-//	    title: 'INSCRIS TOI'
-//	}]
-//    }]
-//});
+Ext.define('Formation.view.Viewport', {
+    renderTo: Ext.getBody(),
+    extend: 'Ext.container.Viewport',
+    requires:[
+    'Ext.tab.Panel',
+    'Ext.layout.container.Border'
+    ],
+
+    layout: {
+	type: 'ux.center'
+    },
+
+    items: [{
+	xtype: 'panel',
+	title: 'BIENVENUE PAR ICI COPAIN',
+	width: 800,
+	items:[{
+	    xtype: 'tabpanel',
+	    title: 'INSCRIS TOI'
+	}]
+    }]
+});
